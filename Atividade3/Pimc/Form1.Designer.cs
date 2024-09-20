392ea3b13ed0b6598c3089080f54c65bc738d1ca
RAUL VICTOR MESSIAS GARCIA PIRES <0030482413057@lti.fatecso.edu.br>
2024-09-20 20:20:12 -0300

    Atividade 3

---
+++ Atividade3/Pimc/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Pimc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblImc = new System.Windows.Forms.Label();
+            this.txtImc = new System.Windows.Forms.TextBox();
+            this.mskbxPeso = new System.Windows.Forms.MaskedTextBox();
+            this.mskbxAltura = new System.Windows.Forms.MaskedTextBox();
+            this.errP = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(145, 291);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(143, 59);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(314, 291);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(143, 59);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(476, 291);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(143, 59);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(51, 75);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(86, 20);
+            this.lblPeso.TabIndex = 3;
+            this.lblPeso.Text = "Peso Atual";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(51, 115);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(51, 20);
+            this.lblAltura.TabIndex = 4;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblImc
+            // 
+            this.lblImc.AutoSize = true;
+            this.lblImc.Location = new System.Drawing.Point(51, 176);
+            this.lblImc.Name = "lblImc";
+            this.lblImc.Size = new System.Drawing.Size(38, 20);
+            this.lblImc.TabIndex = 5;
+            this.lblImc.Text = "IMC";
+            // 
+            // txtImc
+            // 
+            this.txtImc.Enabled = false;
+            this.txtImc.Location = new System.Drawing.Point(233, 176);
+            this.txtImc.Name = "txtImc";
+            this.txtImc.Size = new System.Drawing.Size(100, 26);
+            this.txtImc.TabIndex = 8;
+            this.txtImc.Validating += new System.ComponentModel.CancelEventHandler(this.txtImc_Validating);
+            // 
+            // mskbxPeso
+            // 
+            this.mskbxPeso.Location = new System.Drawing.Point(233, 75);
+            this.mskbxPeso.Mask = "900.00";
+            this.mskbxPeso.Name = "mskbxPeso";
+            this.mskbxPeso.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.mskbxPeso.Size = new System.Drawing.Size(100, 26);
+            this.mskbxPeso.TabIndex = 9;
+            this.mskbxPeso.Click += new System.EventHandler(this.mskbxPeso_Click);
+            this.mskbxPeso.Validating += new System.ComponentModel.CancelEventHandler(this.mskbxPeso_Validating);
+            // 
+            // mskbxAltura
+            // 
+            this.mskbxAltura.Location = new System.Drawing.Point(233, 115);
+            this.mskbxAltura.Mask = "0.00";
+            this.mskbxAltura.Name = "mskbxAltura";
+            this.mskbxAltura.Size = new System.Drawing.Size(100, 26);
+            this.mskbxAltura.TabIndex = 10;
+            this.mskbxAltura.Click += new System.EventHandler(this.mskbxAltura_Click);
+            this.mskbxAltura.Validating += new System.ComponentModel.CancelEventHandler(this.mskbxAltura_Validating);
+            // 
+            // errP
+            // 
+            this.errP.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mskbxAltura);
+            this.Controls.Add(this.mskbxPeso);
+            this.Controls.Add(this.txtImc);
+            this.Controls.Add(this.lblImc);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblImc;
+        private System.Windows.Forms.TextBox txtImc;
+        private System.Windows.Forms.MaskedTextBox mskbxPeso;
+        private System.Windows.Forms.MaskedTextBox mskbxAltura;
+        private System.Windows.Forms.ErrorProvider errP;
+    }
+}
+
---
+++ Atividade3/Pimc/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pimc
+{
+    public partial class Form1 : Form
+    {
+        double peso, altura;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mskbxPeso_Validating(object sender, CancelEventArgs e)
+        {
+            if(!double.TryParse(mskbxPeso.Text, out peso))
+            {
+                errP.SetError(mskbxPeso, "Peso invalido");
+                //e.Cancel = true;
+            }
+           
+        }
+
+        private void mskbxAltura_Validating(object sender, CancelEventArgs e)
+        {
+            if (!double.TryParse(mskbxAltura.Text, out altura))
+            {
+                errP.SetError(mskbxAltura, "Altura invalida");
+                //e.Cancel = true;
+            }
+        }
+
+        private void txtImc_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(mskbxPeso.Text, out peso))
+            {
+                MessageBox.Show("Peso invalido");
+                mskbxPeso.Text = "";
+                mskbxPeso.Focus();
+                return;
+            }
+            if (!double.TryParse(mskbxAltura.Text, out altura))
+            {
+                MessageBox.Show("Altura invalida");
+                mskbxAltura.Text = "";
+                mskbxAltura.Focus();
+                return;
+            }
+            if (altura == 0)
+            {
+                MessageBox.Show("Altura 0 é invalida");
+                mskbxAltura.Text = "";
+                mskbxAltura.Focus();
+                return;
+            }
+            if (peso == 0)
+            {
+                MessageBox.Show("Peso 0 é invalido");
+                mskbxPeso.Text = "";
+                mskbxPeso.Focus();
+                return;
+            }
+            double value = Math.Round((peso / (altura * altura)), 1);
+            txtImc.Text = value.ToString();
+
+            if(value < 18.5)
+            {
+                MessageBox.Show("Magreza");
+                return;
+            }
+            if (value < 25)
+            {
+                MessageBox.Show("Normal");
+                return;
+            }
+            if (value < 30)
+            {
+                MessageBox.Show("Sobrepeso");
+                return;
+            }
+            if (value < 40)
+            {
+                MessageBox.Show("Obesidade");
+                return;
+            }
+            MessageBox.Show("Obesidade Grave");
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            mskbxAltura.Text = "";
+            mskbxPeso.Text = "";
+            txtImc.Text = "";
+        }
+
+        private void mskbxPeso_Click(object sender, EventArgs e)
+        {
+            mskbxPeso.Text = "";
+        }
+
+        private void mskbxAltura_Click(object sender, EventArgs e)
+        {
+            mskbxAltura.Text = "";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
